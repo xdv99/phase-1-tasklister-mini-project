46d815ff41c0ea8cec6963c12d19d08290f3c484
Dawood Alkawaz <dalkawaz@gmail.com>
2024-02-27 23:57:07 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById('create-task-form');
+  const input = document.getElementById('new-task-description');
+  const tasks = document.getElementById('tasks');
+
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    const newItem = document.createElement('li');
+    newItem.textContent = input.value;
+    tasks.appendChild(newItem);
+    input.value = '';
+  })
+
 });
